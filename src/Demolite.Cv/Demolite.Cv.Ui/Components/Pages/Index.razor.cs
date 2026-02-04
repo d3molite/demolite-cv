3db21d0f89bcc698e05ec1c3a941d5436330fc8b
Tobias Brinke <tobias.brinke@cherubim.de>
2026-02-04 14:30:13 +0100

    Work Entry Rendering and Mobile Support

---
+++ src/Demolite.Cv/Demolite.Cv.Ui/Components/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using Demolite.Cv.Interfaces.Models;
+using Demolite.Cv.Interfaces.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Demolite.Cv.Ui.Components.Pages;
+
+public partial class Index
+{
+	[Inject]
+	public ICvEntryService CvEntryService { get; set; } = null!;
+
+	private ICvEntry[] CvEntries { get; set; } = [];
+
+	protected override async Task OnInitializedAsync()
+	{
+		CvEntries = await CvEntryService.GetCvEntries();
+	}
+}
