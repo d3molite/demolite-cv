e1485eaab07e3196aedd44c8169aa336e62e442b
Tobias Brinke <tobias.brinke@cherubim.de>
2026-02-04 17:20:30 +0100

    Mobile Adjustments and Publishing

--- src/Demolite.Cv/Demolite.Cv/Program.cs
+++ src/Demolite.Cv/Demolite.Cv/Program.cs
@@ -16,10 +16,6 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-else
-{
-	app.UseHttpsRedirection();
-}
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseAntiforgery();
